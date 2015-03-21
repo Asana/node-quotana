26d9305fc87127ad23705cf9b8af14248927b496
greg <greg@asana.com>
2015-03-21 01:27:27 -0700

    Big refactor of quote loading allowing for dynamic update of webapp data

--- import.js
+++ import.js
@@ -11,12 +11,15 @@ config(process.env.QUOTANA_CONFIG);
  * Helper script to import existing JSON quotes into Asana.
  */
 
+var dryRun = false;
+var multi = true;
 var args = process.argv.slice(2);
 var filename = args[0];
 var projectId = parseInt(args[1], 10);
 var startOffset = parseInt(args[2] || 0, 10);
+var limit = parseInt(args[3] || 0, 10);
 if (typeof(filename) !== 'string' || isNaN(projectId)) {
-  console.log('Usage: node import.js QUOTEFILE PROJECT_ID [OFFSET]');
+  console.log('Usage: node import.js QUOTEFILE PROJECT_ID [OFFSET] [LIMIT]');
   process.exit(1);
 }
 
@@ -35,23 +38,55 @@ console.log(
         quotesFromFile.length, startOffset, projectId, project.type));
 
 var client = Asana.Client.create(config);
-client.useOauth({ credentials: { refresh_token: config.refreshToken }});
+//client.useOauth({ credentials: { refresh_token: config.refreshToken }});
+client.useBasicAuth('ErQ69.iLYR2QNGWdvKojzVgTjqUKHTwk');
 
 var index = startOffset;
 var promise = Bluebird.resolve();
-quotesFromFile.slice(startOffset).forEach(function(quote) {
+quotesFromFile.slice(startOffset, startOffset + (limit || 10000)).forEach(function(quote) {
   promise = promise.then(function() {
     console.log(index, quote);
     index++;
+
+    if (quote.unsafe) {
+      return Bluebird.resolve();
+    }
+
     var data = {
       workspace: config.workspaceId,
-      name: quote.author,
-      notes: quote.quote + '\n' + quote.date,
       projects: [projectId]
     };
-    return client.tasks.create(data).catch(function(err) {
-      console.log(err.value);
-      throw err;
-    });
+    if (multi) {
+      data.notes = '';
+      var authors = [];
+      var authorsSeen = {};
+      for (var i = 0; i < quote.quote.length; i += 2) {
+        var author = quote.quote[i];
+        if (!(author in authorsSeen)) {
+          authorsSeen[author] = true;
+          authors.push(author);
+        }
+        var line = quote.quote[i + 1];
+        data.notes += author + ': ' + line + '\n';
+      }
+      if (quote.context) {
+        data.notes += quote.context + '\n';
+      }
+      data.notes += '\n' + quote.date + '\n';
+      data.name = quote.date + ' ' + authors.join(' and ');
+    } else {
+      data.name = quote.author;
+      data.notes = quote.quote + '\n' + quote.date;
+    }
+
+    if (dryRun) {
+      console.log('Would post:', data);
+      return Bluebird.resolve();
+    } else {
+      return client.tasks.create(data).catch(function(err) {
+        console.log(err.value);
+        throw err;
+      });
+    }
   });
 });
